2b7f0f518b027611d42cd1f371315211c0d40e16
David J. Manzano <davidjosemanzano@gmail.com>
2023-12-26 06:24:09 -0500

    Actualizacion 12/26/2023

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import {dirname, join} from "path"
 import {fileURLToPath} from "url"
-import indexRoutes from './routes/index.js'
+import indexRoutes from './routes/index_routes.js'
 
 
 
